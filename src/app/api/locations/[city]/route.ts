7334c1284fffe3a548bca359b1355d1af1269c80
HaimIshta <95882785+Haim02@users.noreply.github.com>
2025-08-03 19:58:18 +0300

    fix error

--- src/app/api/locations/[city]/route.ts
+++ src/app/api/locations/[city]/route.ts
@@ -1,9 +1,12 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: Request, { params }: { params: { city: string | any }} ) {
+export async function GET(req: NextRequest, context: { params: { city: string } }) {
+  const { city } = context.params;
+
   const res = await fetch(
-    `https://data.gov.il/api/3/action/datastore_search?resource_id=a7296d1a-f8c9-4b70-96c2-6ebb4352f8e3&q=${encodeURIComponent(params.city)}&limit=2000`
+    `https://data.gov.il/api/3/action/datastore_search?resource_id=a7296d1a-f8c9-4b70-96c2-6ebb4352f8e3&q=${encodeURIComponent(city)}&limit=2000`
   );
+
   const json = await res.json();
   return NextResponse.json(json);
 }
