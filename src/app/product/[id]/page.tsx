1e64b044f69bd4598ecccc04b4dfe0c41319e0d2
HaimIshta <95882785+Haim02@users.noreply.github.com>
2025-12-14 05:39:45 +0200

    fix db connect

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
 import ProductDetails from "@/components/products/ProductDetails";
 
-const ProductDetailsPage = async ({params}: {params: { id: string }}) => {
-  const productId = params.id;
+const ProductDetailsPage = async ({params}: {params: Promise<{ id: string }>}) => {
+  const productId = (await params).id;
 
   return <ProductDetails productId={productId} />;
 };
