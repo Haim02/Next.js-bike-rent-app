029566c1dc54f0e8bd5c71ea797083ab72e4a1c4
HaimIshta <95882785+Haim02@users.noreply.github.com>
2025-12-14 05:25:28 +0200

    fix db connect

--- src/app/api/locations/[city]/route.ts
+++ src/app/api/locations/[city]/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
 
 
-export async function GET(req: Request, { params }: { params: { city: string | any } } ) {
-  const { city } =  params;
+export async function GET(req: Request, { params }: { params: Promise<{ city: string | any }> } ) {
+  const { city } = await params;
 
   const res = await fetch(
     `https://data.gov.il/api/3/action/datastore_search?resource_id=a7296d1a-f8c9-4b70-96c2-6ebb4352f8e3&q=${encodeURIComponent(city)}&limit=2000`
