04ebbaf65a1f080ac8905913152304b550f72a4e
HaimIshta <95882785+Haim02@users.noreply.github.com>
2025-12-15 14:58:09 +0200

    fix db connect

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,27 @@
-import { PrismaClient } from "@/generated/prisma";
+// import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// export const db =
+//   globalForPrisma.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+
+// src/lib/db.ts
+import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@src/generated/prisma";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const db =
-  globalForPrisma.prisma || new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
